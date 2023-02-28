d3a3d5b51dfa21bf18cddadd07144363abb83964
kjones <kvjones0243@proton.me>
2023-02-28 00:19:06 -0800

    ota: json-gen: optimize the script for ota generation
    This commit refactors the `generate_ota_json.sh` script for generating OTA JSON
    files for custom Android ROMs. The changes include:
    
    - Using `printf` instead of `echo`
    - Using consistent naming conventions
    - Adding a check for `jq` before generating the JSON file
    
    These changes improve the script's performance, readability, and maintainability.

--- OTAJsonGenerator.sh
+++ OTAJsonGenerator.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 #
-# JMPFBMX (C) 2021
-#
+# This script generates a JSON file for a ROM build.
 
-e=echo
-
-$e -n "CODENAME: "
-CODENAME="$1"
-read CODENAME
-$e -n "ROM: "
-ROM="$2"
-read ROM
-$e -n "ROM_VERSION: "
-ROM_VERSION="$3"
-read ROM_VERSION
-$e -n "ROM_BUILDTYPE: "
-ROM_BUILDTYPE="$4"
-read ROM_BUILDTYPE
-$e -n "DOWNLOAD_LINK: "
-DOWNLOAD_LINK="$5"
-read DOWNLOAD_LINK
-
-OUT="out/target/product/$CODENAME"
-FILENAME=$(ls $OUT/$ROM-$ROM_VERSION-*-$ROM_BUILDTYPE-$CODENAME.zip | cut -f5 -d '/')
-DATETIME=$(cat $OUT/system/build.prop | grep ro.build.date.utc | cut -f2 -d '=')
-MD5SUM=$(cat $OUT/$ROM-$ROM_VERSION-*-$ROM_BUILDTYPE-$CODENAME.zip.md5sum | cut -f1 -d ' ')
-SIZE=$(du -b $OUT/$ROM-$ROM_VERSION-*-$ROM_BUILDTYPE-$CODENAME.zip | cut -f1 -d '	')
-if ( ! ls | grep ./$ROM\_$CODENAME.json);
-then
-    $e -e "File doesn't exists!\nWait we are generating one"
-    touch ./$ROM"_"$CODENAME."json"
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Please install it before running this script."
+    exit 1
 fi
 
-$e -e "{
-  \"response\": [
-    {
-      \"datetime\": \"$DATETIME\",
-      \"filename\": \"$FILENAME\",
-      \"id\": \"$MD5SUM\",
-      \"romtype\": \"$ROM_BUILDTYPE\",
-      \"size\": \"$SIZE\",
-      \"url\": \"$DOWNLOAD_LINK\",
-      \"version\": \"$ROM_VERSION\"
-    }
-  ]
-}" > $ROM"_"$CODENAME".json"
+printf "CODENAME: "
+read codename
+printf "ROM: "
+read rom
+printf "ROM_VERSION: "
+read rom_version
+printf "ROM_BUILDTYPE: "
+read rom_buildtype
+printf "DOWNLOAD_LINK: "
+read download_link
 
-cat $ROM"_"$CODENAME."json"
+out="out/target/product/$codename"
+filename=$(ls "$out"/"$rom"-"$rom_version"-*-"$rom_buildtype"-"$codename".zip | cut -f5 -d '/')
+datetime=$(cat "$out"/system/build.prop | grep ro.build.date.utc | cut -f2 -d '=')
+md5sum=$(cat "$out"/"$rom"-"$rom_version"-*-"$rom_buildtype"-"$codename".zip.md5sum | cut -f1 -d ' ')
+size=$(du -b "$out"/"$rom"-"$rom_version"-*-"$rom_buildtype"-"$codename".zip | cut -f1 -d '	')
+json_file="$rom"_"$codename".json
+
+if [ ! -f "$json_file" ]; then
+  printf "File doesn't exist!\nGenerating...\n"
+fi
+
+jq --arg datetime "$datetime" \
+   --arg filename "$filename" \
+   --arg id "$md5sum" \
+   --arg romtype "$rom_buildtype" \
+   --arg size "$size" \
+   --arg url "$download_link" \
+   --arg version "$rom_version" \
+   '{response: [{datetime: $datetime, filename: $filename, id: $id, romtype: $romtype, size: $size, url: $url, version: $version}]}' \
+   > "$json_file"
+
+cat "$json_file"
